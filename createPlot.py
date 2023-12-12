5dfdf21d27140b0384e780d91f5704c35fc4c797
Fabio Gianzanti <fabio.gianzanti@jbtec.com.br>
2023-12-12 13:44:56 -0300

    feat(doc): jupyter notebook creation

--- createPlot.py
+++ createPlot.py
@@ -1,19 +1,8 @@
-# load data from npz
-# plot data
-
 import numpy as np
 import matplotlib.pyplot as plt
 from icecream import ic
 
-# load data from npz
 data = np.load("data_grid.npz")
-# data = np.load("data_history.npz")
-# ic(data.files)
-# ic(data["grid"])
-
-# new_grid = data["grid"].copy()
-# width = new_grid.shape[0]
-# height = new_grid.shape[1]
 
 max_value = np.max(data["grid"])
 ic(max_value)
@@ -22,60 +11,12 @@ ic(min_value)
 mean_value = np.mean(data["grid"])
 ic(mean_value)
 
-# self.thresholdFree = -3000
-# self.thresholdOccupied = 500
-
-# Zero out the elements of the copied array
-# new_grid.fill(0)
-
-# for x in range(0, width):
-#     for y in range(0, height):
-#         if data["grid"][x][y] > mean_value:
-#             new_grid[x][y] = -10
-#         elif data["grid"][x][y] < mean_value:
-#             new_grid[x][y] = 10
-#         # else:
-#         #     new_grid[x][y] = 10
-
-# ic(new_grid)
-
-
-# xy_res = np.array(new_grid).shape
-# plt.figure(1, figsize=(10, 4))
-# plt.subplot(122)
-# plt.imshow(new_grid, cmap="bone")
-# plt.clim(-10, 10)
-# plt.gca().set_xticks(np.arange(-0.5, xy_res[1], 1), minor=True)
-# plt.gca().set_yticks(np.arange(-0.5, xy_res[0], 1), minor=True)
-# plt.grid(True, which="minor", color="w", linewidth=0.6, alpha=0.5)
-# plt.colorbar()
-# # plt.subplot(121)
-# # plt.plot([oy, np.zeros(np.size(oy))], [ox, np.zeros(np.size(oy))], "ro-")
-# # plt.axis("equal")
-# # plt.plot(0.0, 0.0, "ob")
-# # plt.gca().set_aspect("equal", "box")
-# # bottom, top = plt.ylim()  # return the current y-lim
-# # plt.ylim((top, bottom))  # rescale y axis, to match the grid orientation
-# # plt.grid(True)
-# plt.show()
-
-
-# plt.figure(1, figsize=(10, 4))
-# plt.subplot(122)
-# plt.imshow(new_grid, cmap="bone_r")
-
 fig, ax = plt.subplots(1, 1, figsize=(20, 6))
 
 ax.set_ylabel("Posição Y [0.25 m cada]")
 ax.set_xlabel("Posição X [0.25 m cada]")
 ax.set_title("Grid de Ocupação", fontsize=14)
-
-# ax.set_xlim(-5, 5)
-# ax.set_ylim(0, 6)
 ax.set_aspect("equal", "box")
-# ax.tight_layout()
-# ax.grid()
-
 
 new_grid = np.rot90(data["grid"], k=3)
 new_grid = np.fliplr(new_grid)
@@ -84,10 +25,5 @@ ax.pcolor(
     cmap="bone_r",
     edgecolor="tab:gray",
     linewidths=1,
-    # vmax=max_value * 0.2,
-    # vmin=min_value * 0.8,
-)  # , vmin=0.0, vmax=1.0)
-# plt.title("Occupancy Grid")
-# plt.tight_layout()
-# plt.axis("equal")
+)
 plt.show()
